2711bfb2ac2d529f908fcf886be58820e4bb66a1
leonardoricardodev98 <leonardoricardo2087@gmail.com>
2026-06-05 12:42:14 -0300

    commit inicial

---
+++ src/test/java/com/leonardoricardo/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoricardo.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
