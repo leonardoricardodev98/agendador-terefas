5054bfba45d410c2471ef5bc8c76fc158a0f59df
leonardoricardodev98 <leonardoricardo2087@gmail.com>
2026-06-13 12:12:58 -0300

    correção de apontamentos

--- src/main/java/com/leonardoricardo/agendadortarefas/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/leonardoricardo/agendadortarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -2,13 +2,9 @@ package com.leonardoricardo.agendadortarefas.infrastructure.security;
 
 import com.leonardoricardo.agendadortarefas.business.dto.UsuarioDTO;
 import com.leonardoricardo.agendadortarefas.infrastructure.client.UsuarioClient;
-import com.leonardoricardo.usuario.infrastructure.entity.Usuario;
-import com.leonardoricardo.usuario.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
